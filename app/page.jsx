166e14d7ac3830d42a6ee998240fc161c33e6ca2
f4zza <lazie.piscean@live.co.uk>
2023-05-23 18:19:37 +0300

    imported feed

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from "@components/Feed";
+
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
@@ -6,6 +8,8 @@ const Home = () => {
         <span className="orange_gradient text-center"> AI Powered Prompts</span>
         </h1>
         <p className="desc text-center">Promptville is an open-source AI prompting tool for everyone to Discover, create & share unique prompts.</p>
+
+        <Feed />
     </section>
   )
 }
