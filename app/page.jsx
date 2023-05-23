33b23757f50c868d528b148243b523e4d28de751
f4zza <lazie.piscean@live.co.uk>
2023-05-23 18:12:35 +0300

    promptville initial

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">Discover & Share AI Prompts.
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> AI Powered Prompts</span>
+        </h1>
+        <p className="desc text-center">Promptville is an open-source AI prompting tool for everyone to Discover, create & share unique prompts.</p>
+    </section>
+  )
+}
+
+export default Home
