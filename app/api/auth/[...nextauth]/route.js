8919c72b1e8e973d0ec8f16952525f5d91d80c6f
f4zza <lazie.piscean@live.co.uk>
2023-05-25 00:26:58 +0300

    updated schema

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,6 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { signIn } from "next-auth/react";
 
 import { connectToDB} from '@utils/database'
+import User from '@models/user'
 
 
 
@@ -20,8 +21,23 @@ const handler = nextAuth({
         async signIn({ profile }){
             try {
                 await connectToDB()
+
+                //check if the user already exists
+                const userExists = await User.findOne({
+                    email: profile.email
+                })
+                //create if not
+                if(!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ","").toLowerCase(),
+                        image: profile.picture
+                    })
+                }
+                return true
             } catch (error) {
-                
+                console.log(error)
+                return false
             }
         }
 })
